144be2313f9e9d32dda4d96a360f2f4012ca99e7
陈新宇 <2279428879@qq.com>
2023-12-19 09:07:21 +0800

    feat: 添加代码美化插件

--- src/components/CopyTextarea.vue
+++ src/components/CopyTextarea.vue
@@ -2,13 +2,14 @@
   <section class="">
     <div class="mb-10px h-18px flex items-center text-left text-16px font-400 text-#333">
       <div class="mr-10px h-18px w-4px bg-red-400 leading-18px"></div>
-      <span class="">{{ title }}</span>
+      <span>{{ title }}</span>
       <CopyOutlined
         class="ml-10px mt-2px cursor-pointer text-14px"
         @click="copyText(text, copySuccessMsg)"
       />
     </div>
-    <a-textarea :value="text" :autoSize="{ minRows: 5, maxRows: 15 }" />
+    <highlightjs language="javascript" :code="text" />
+    <!--    <a-textarea :value="text" :autoSize="{ minRows: 5, maxRows: 15 }" />-->
   </section>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,27 @@ import App from './App.vue'
 import './style.css'
 import { registerXhrWatcher } from './utils/onLoadXhr'
 
+/*黑色主题*/
+import 'highlight.js/styles/atom-one-dark.css'
+/*白色主题*/
+import 'highlight.js/styles/stackoverflow-light.css'
+import hljs from 'highlight.js/lib/core'
+import javascript from 'highlight.js/lib/languages/javascript'
+import hljsVuePlugin from '@highlightjs/vue-plugin'
+
+hljs.registerLanguage('javascript', javascript)
+
 registerXhrWatcher()
 
 window.onload = () => {
-  console.log('load')
-  createApp(App).mount(
-    // @ts-ignore
-    (() => {
-      const dom = document.createElement('div')
-      document.body!.append(dom)
-      return dom
-    })()
-  )
+  createApp(App)
+    .use(hljsVuePlugin)
+    .mount(
+      // @ts-ignore
+      (() => {
+        const dom = document.createElement('div')
+        document.body!.append(dom)
+        return dom
+      })()
+    )
 }
